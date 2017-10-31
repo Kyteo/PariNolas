323b798da5761e718e452c478cfbffebac0b32e1
pshearzad <pshearzad@192.168.0.106>
2017-10-31 00:49:23 -0400

    Création du folder version client

---
+++ Client/accueilClient.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<title>Accueil version client PariNolas</title>
+	<meta charset="UTF-8">
+	<link href="designClient.css" type="text/css" rel="stylesheet" />
+
+
+</head>
+
+<body>
+	
+	<?php include 'entete.php'; ?>
+	
+	<br>
+	<div class="accueil">
+		<img src="/Client/images/couple1.png" alt="Couple">
+		<img src="/Client/images/couple2.png" alt="Couple">
+		<img src="/Client/images/groupe.png" alt="Couple">
+	</div>
+	
+</body>
+
+</html>
