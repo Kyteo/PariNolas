cb0958e8af56c4b0cfa112cdeb885219d8a230a7
pshearzad <pshearzad@172.29.108.172>
2017-12-08 13:42:52 -0500

    Test autre accueil

--- Client/accueilClient.php
+++ Client/accueilClient.php
@@ -1,3 +1,7 @@
+<?php
+	include 'validationInfolettre.php';
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -13,12 +17,12 @@
 <body>
 	
 	<?php include 'entete.php'; ?>
-	<?php echo 'TEst'; ?>
+	
 	<br><br>
 	<div id="pageAccueil">
 		<div id="lignes">
-			<div id="marquee"><marquee behavior="scroll" width="400px" scrollamount="10" scrolldelay="3"><pre id="titreGauche">Recréez votre style avec notre nouvelle collection signée PariNolas!                                  Livraison gratuite pour les commandes de 50$ et plus!</pre></marquee></div>
-			<!--<div id"titreDroite">Livraison gratuite pour les commandes de 50$ et plus!</div>-->
+			<div id="marquee"><marquee behavior="scroll" width="800px" scrollamount="5" scrolldelay="3"><pre id="banniere">Recréez votre style avec notre nouvelle collection signée PariNolas!                                  Livraison gratuite pour les commandes de 50$ et plus!</pre></marquee></div>
+
 		</div><br><br><br>
 	
 		<div class="accueil">
@@ -37,11 +41,18 @@
 		
 		</div><br>
 		
+		<!-- Infolettre -->
 		<div class="infolettre">
 			<h3>Inscrivez-vous à notre infolettre pour recevoir les meilleures promotions!</h3>
-			<form method="post">
-				<input type="text" placeholder="adresse@example.com" size="40"><br>
-				<button class="infolettre">M'inscrire</button>
+			<form method="post" action="accueilClient.php">
+				<?php 
+					echo "<input type='text' value='{$courriel}' placeholder='adresse@example.com' name='courriel' size='40'><br><br>";
+				 
+					if ($message != "") {
+				    	echo "<p>{$message}</p>";
+					}
+				?>
+				<input type="submit" id ="infolettre" name="infolettre" value="M'inscrire!">
 			</form>
 		</div>
 	
